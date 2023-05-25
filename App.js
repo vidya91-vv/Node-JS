f582c010b5aef8c7cf94360ed5d3823c44cb8e96
vidya91-vv <venvidyashree@gmail.com>
2023-05-25 22:57:42 +0530

    okie

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const app = express()
 
+const app = express()
 app.get('/',(req, res)=>{
   res.send("Root Request")
 })
@@ -8,3 +8,13 @@ app.get('/',(req, res)=>{
   if(err) throw err
   console.log('Server Running on port Number: ${5000}')
 })
+
+/*
+  How to read Application cofiguration encv variables;
+ 
+  1. install dotenv
+ * npm i dotenv
+  2. import dotenv
+  3. provide your configuration path.
+ * dotenv.config({path:'})
+ */
